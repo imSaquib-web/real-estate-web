e5a236da105beafb772588b80dc60665aee7f582
imSaquib-web <saquib1230@gmail.com>
2026-01-06 18:06:10 +0530

    hello

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_API_URL,
+  baseURL: `${import.meta.env.VITE_SERVER_API_URL}/api`,
   
 });
-console.log("API URL:", import.meta.env.VITE_SERVER_API_URL);
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
